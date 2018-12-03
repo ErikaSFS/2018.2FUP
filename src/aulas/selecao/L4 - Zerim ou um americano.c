3b1a464cc5c52edf696ac89ef146b497e6386a99
Erika Faustino <dossantos.erika08@gmail.com>
2018-12-03 08:30:37 -0300

    Create L4 - Zerim ou um americano.c

---
+++ src/aulas/selecao/L4 - Zerim ou um americano.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+    int jog1, jog2, jog3, jog4;
+    scanf("%d", &jog1);
+    scanf("%d", &jog2);
+    scanf("%d", &jog3);
+    scanf("%d", &jog4);
+    
+    if(jog1 ==0 && jog2==0 && jog3==0 && jog4==0){
+        printf("nenhum");
+    }
+
+    
+}
