0d770e1be8e1cc315a16f124ffd390a9ffd1c953
Erika Faustino <dossantos.erika08@gmail.com>
2019-04-03 09:05:55 -0300

    Create verificar_letra.c

---
+++ src/aulas/switch/verificar_letra.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+  char letra;
+
+  scanf(" %c", &letra);
+
+  switch (letra){
+    
+case 'a':
+  printf("eh vogal");
+  break;
+case 'e':
+  printf("eh vogal");
+  break;
+case 'i':
+  printf("eh vogal");
+  break;
+case 'o':
+  printf("eh vogal");
+  break;
+case 'u':
+  printf("eh vogal");
+  break;
+default:
+  printf("nao eh vogal");
+  }
+  return 0;
+}
